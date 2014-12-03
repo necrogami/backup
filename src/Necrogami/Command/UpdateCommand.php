a55eca123213cffb307d76dbfad0c91918efcbc4
Anton Swartz IV <aswartz@whitecloudcigs.com>
2014-12-03 14:29:19 -0500

    Adding Update and Updating

---
+++ src/Necrogami/Command/UpdateCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Necrogami\Command;
+
+use Herrera\Phar\Update\Manager;
+use Herrera\Phar\Update\Manifest;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UpdateCommand extends Command
+{
+    const MANIFEST_FILE = 'http://necrogami.github.io/backup/manifest.json';
+
+    protected function configure()
+    {
+        $this
+            ->setName('update')
+            ->setDescription('Updates backup.phar to the latest version')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $manager = new Manager(Manifest::loadFile(self::MANIFEST_FILE));
+        $manager->update($this->getApplication()->getVersion(), true);
+    }
+}
